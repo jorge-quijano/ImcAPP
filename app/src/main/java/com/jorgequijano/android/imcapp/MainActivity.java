0ab0533808fbff19a8d23142e1bbb1504a9c10ee
jorge roman quijano calan <13470173>
2017-12-19 00:42:50 -0600

    Proyecto ImcApp que calcula el indice de masa corporal

--- app/src/main/java/com/jorgequijano/android/imcapp/MainActivity.java
+++ app/src/main/java/com/jorgequijano/android/imcapp/MainActivity.java
@@ -8,7 +8,6 @@ import android.os.Bundle;
 
 public class MainActivity extends FragmentActivity {
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
